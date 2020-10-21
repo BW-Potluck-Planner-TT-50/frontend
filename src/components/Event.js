59b33a670a9b1c237ee53fa10c34e72d74d9ddfe
Joon <jong@jongs-mbp.lan>
2020-10-20 20:25:37 -0700

    fixed bugs

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { axiosWithAuth } from '../utils/axiosWithAuth'
-import { useHistory, useParams, NavLink, Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 
 function Event({ deleteFromEventList, eventList }) {
-
-   const params = useParams()
-   const history = useHistory()
 
    const deleteEvent = (id) => {
 
