a75a56621a7247eefc89a092534a8a7592d92e25
Joon <jong@jongs-mbp.lan>
2020-10-21 11:32:03 -0700

    fix buggs adding/deleting guest and food list

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -5,14 +5,28 @@ import { axiosWithAuth } from '../utils/axiosWithAuth'
 const EventCard = () => {
 
    const params = useParams()
+   console.log(params)
+
+   // Initial State
    const [potEvent, setPotEvent] = useState('')
 
    const [food, setFood] = useState({
-      id: params.id,
+      id: '',
       name: '',
    })
-   const [foodList, setFoodList] = useState([])
 
+   const [guest, setGuest] = useState({
+      id: '',
+      name: '',
+   })
+
+   const [guestList, setGuestList] = useState([])
+   const [foodList, setFoodList] = useState([])
+   
+   console.log('FOOD LIST: \n', foodList, '\nGUEST LIST: \n', guestList)
+
+
+   // fetch initial food and guest list
    useEffect(() => {
 
       // get event information
@@ -29,8 +43,15 @@ const EventCard = () => {
             setFoodList(res.data)
          })
 
+      axiosWithAuth()
+         .get(`/api/events/${params.id}/guest-list`)
+         .then(res => {
+            setGuestList(res.data)
+         })
+
    },[params.id])
 
+   // Food CRUD
    const deleteFood = (id) => {
 
       axiosWithAuth()
@@ -41,30 +62,61 @@ const EventCard = () => {
       setFoodList(foodList.filter((eachFood) => eachFood.id !== id))
    }
 
-   const addFood = (food) => {
-      setFoodList([...foodList, food])
-   }
-
-   const handleChange = (e) => {
+   const handleFoodChange = (e) => {
       setFood({
          ...food,
          [e.target.name]: e.target.value
       })
    }
 
-   const handleSubmit = (e) => {
+   const handleFoodSubmit = (e, id) => {
 
       e.preventDefault()
       // add food to the list of food
       axiosWithAuth()
          .post(`/api/events/${params.id}/food`, food)
          .then(res => {
-            console.log(res)
+            setFoodList([...foodList, res.data])
          })
       
       // reset food state
       setFood({
          ...food,
+         name: '',
+      })
+   }
+
+   // Guest CRUD
+
+   const deleteGuest = (id) => {
+      axiosWithAuth()
+         .delete(`/api/events/guest-list/${id}`)
+         .then(res => {
+            console.log(res)
+         })
+      setGuestList(guestList.filter((eachGuest) => eachGuest.id !== id))
+   }
+
+   const handleGuestChange = (e) => {
+      setGuest({
+         ...guest,
+         [e.target.name]: e.target.value
+      })
+   }
+
+   const handleGuestSubmit = (e) => {
+
+      e.preventDefault()
+      // add food to the list of food
+      axiosWithAuth()
+         .post(`/api/events/${params.id}/guest-list`, guest)
+         .then(res => {
+           setGuestList([...guestList, res.data])
+         })
+      
+      // reset food state
+      setGuest({
+         ...guest,
          name: '',
       })
    }
@@ -76,10 +128,26 @@ const EventCard = () => {
          <p>Time: {potEvent.time}</p>
          <p>Location: {potEvent.location}</p>
          <p>Invite Code: {potEvent.invite_code}</p>
-         <form onSubmit={handleSubmit}>
-            <label htmlFor='food'>
-               <input name='name' onChange={handleChange} value={food.name}/>
-               <button onClick={() => addFood(food)}>Add Food</button>
+         <form onSubmit={handleGuestSubmit}>
+            <label htmlFor='name'>
+               <input name='name' onChange={handleGuestChange} value={guest.name}/>
+               <button>Add Guest</button>
+            </label>
+         </form>
+         {
+            guestList.map((eachGuest) => {
+               return (
+                  <div key={eachGuest.name}>
+                     <div>{eachGuest.name}</div>
+                     <div onClick={() => deleteGuest(eachGuest.id)}>X</div>
+                  </div>
+               )
+            })
+         }
+         <form onSubmit={handleFoodSubmit}>
+            <label htmlFor='name'>
+               <input name='name' onChange={handleFoodChange} value={food.name}/>
+               <button>Add Food</button>
             </label>
          </form>
          {
