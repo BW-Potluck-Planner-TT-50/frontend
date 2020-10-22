43274c8fa4dfca32be9f8a4f90223fcf749e81b4
Caroline <carolinefallonlucas@gmail.com>
2020-10-22 17:36:26 -0400

    final -ish updates to login and event add

--- src/components/EventGuest.js
+++ src/components/EventGuest.js
@@ -15,7 +15,7 @@ const StyledEventGuest = styled.div`
     height: 100%;
     text-align: center;
     form {
-      background-color: #F0A202;
+      background-color: #8B475D;
       box-shadow: 3px 3px 5px black;
       padding: 3%;
       min-width: 35%;
@@ -59,80 +59,86 @@ const StyledEventGuest = styled.div`
 `
 
 const schema = yup.object().shape({
-  invite_code: yup.string().required("You Must Enter An Event Code To Continue").min(7, "Must be at least 7 characters"),
-  name: yup.string().required("You must enter your name")
+    invite_code: yup.string().required("You Must Enter An Event Code To Continue").min(7, "Must be at least 7 characters"),
+    name: yup.string().required("You must enter your name")
 })
 
 
-function EventGuest({ setIsOrganizer, setLoggedIn }) {
+function EventGuest({ setIsOrganizer, setLoggedIn })
+{
 
-  const [form, setForm] = useState({
-    name: "",
-    invite_code: "",
-  })
-
-  const history = useHistory()
-
-  const [disabled, setDisabled] = useState(true)
-  const [errors, setErrors] = useState({  invite_code: "" })
-  
-  const setFormErrors = (name, value) => {
-    yup.reach(schema, name).validate(value)
-    .then(() => setErrors({...errors, [name]: ''}))
-    .catch(err => setErrors({...errors, [name]: err.errors[0]}))
-  }
-
-  function handleChange(e) {
-    const { value, name} = e.target
-    setFormErrors(name, value)
-    setForm({ ...form, [name]: value})
-  }
-
-  useEffect(() => {
-    schema.isValid(form).then(valid => {
-      const submit = document.querySelector('#submit')
-      !valid ? submit.classList.add('disabled') : submit.classList.remove('disabled')
-      setDisabled(!valid)
+    const [form, setForm] = useState({
+        name: "",
+        invite_code: "",
     })
-  }, [form])
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    axiosWithAuth()
-      .post("/api/auth-guest/login", form)
-      .then((res) =>
-      {
-        localStorage.setItem("token", res.data.token);
+    const history = useHistory()
 
-        localStorage.setItem("organizer", false);
-        setLoggedIn(true)
-        setIsOrganizer(false)
-        
-        history.push("/plan");
-      })
-  };
-  
+    const [disabled, setDisabled] = useState(true)
+    const [errors, setErrors] = useState({ invite_code: "" })
 
-  return (
-    <StyledEventGuest>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <h1>Join An Event!</h1>
-          <h2>Enter Your Name and Event Code Below</h2>
-          <div className="errors">{errors.name}</div>
-          <div>
-            <input autoComplete="off" placeholder="Name" name="name" value={form.name} onChange={handleChange} />
-          </div>
-          <div className="errors">{errors.invite_code}</div>
-          <div>
-            <input autoComplete="off" placeholder="Event Code" name="invite_code" value={form.invite_code} onChange={handleChange} />
-          </div>
-          <button disabled={disabled} id="submit">Submit</button>
-        </form>
-      </div>
-      
-    </StyledEventGuest>
-  )
+    const setFormErrors = (name, value) =>
+    {
+        yup.reach(schema, name).validate(value)
+            .then(() => setErrors({ ...errors, [name]: '' }))
+            .catch(err => setErrors({ ...errors, [name]: err.errors[0] }))
+    }
+
+    function handleChange(e)
+    {
+        const { value, name } = e.target
+        setFormErrors(name, value)
+        setForm({ ...form, [name]: value })
+    }
+
+    useEffect(() =>
+    {
+        schema.isValid(form).then(valid =>
+        {
+            const submit = document.querySelector('#submit')
+            !valid ? submit.classList.add('disabled') : submit.classList.remove('disabled')
+            setDisabled(!valid)
+        })
+    }, [form])
+
+    function handleSubmit(e)
+    {
+        e.preventDefault()
+        axiosWithAuth()
+            .post("/api/auth-guest/login", form)
+            .then((res) =>
+            {
+                localStorage.setItem("token", res.data.token);
+
+                localStorage.setItem("organizer", false);
+                setLoggedIn(true)
+                setIsOrganizer(false)
+
+                history.push("/plan");
+            })
+    };
+
+
+    return (
+        <StyledEventGuest>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <h1>Join An Event!</h1>
+                    <h2>Enter Your Name and Event Code Below</h2>
+                    <div className="errors">{errors.name}</div>
+                    <div>
+                        <input autoComplete="off" placeholder="Name" name="name" value={form.name} onChange={handleChange} />
+                    </div>
+                    <div className="errors">{errors.invite_code}</div>
+                    <div>
+                        <input autoComplete="off" placeholder="Event Code" name="invite_code" value={form.invite_code} onChange={handleChange} />
+                    </div>
+                    <button disabled={disabled} id="submit">Submit</button>
+                </form>
+            </div>
+
+        </StyledEventGuest>
+    )
 }
 
 export default EventGuest
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { useHistory } from "react-router";
+import { Link, useHistory } from "react-router-dom";
 import schema from "./loginValidation";
 import * as yup from "yup";
+import styled from "styled-components"
+import { Card, Container, Col, Button, Form, Input } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import './Login.scss'
 
-const blankData = {
-    username: "",
-    password: "",
-};
-const errorStrings = {
-    username: "",
-    password: "",
-};
+
+const StyledLogin = styled.div`
+  background-color: #f88e79;
+  width: 60vw;
+  height: 70vh;
+  margin: 15vh auto;
+    .login-container {
+      display: flex;
+      text-align: center;
+      form {
+        background-color: #202C59;
+        box-shadow: 3px 3px 3px black;
+        padding: 3%;
+        min-width: 35%;
+        color: white;
+        text-shadow: 1px 1px 1px black;
+        .errors {
+          height: 20px;
+          color: red;
+          font-weight: bold;
+          font-size: 1.2rem;
+          width: 400px;
+          margin: 0px auto;
+        }
+        input {
+          margin: 3%;
+          padding: 2%;
+          text-align: center;
+          font-size: 1.5rem;
+        }
+        button {
+            font-size: 1em;
+            margin: 1em;
+            padding: 0.25em 1em;
+            border-radius: 3px;
+            background-color: #581F18;
+            color:olive;
+        }
+        .disabled {
+          color: rgba(16, 16, 16, 0.3);
+          background-color: rgba(239, 239, 239, 0.3);
+          border: 0px solid transparent;
+          box-shadow: 0px 0px 0px black;
+        
+        }
+    }
+    }
+    `
+
+
 
 function Login({ setLoggedIn, setIsOrganizer })
 {
-    const [userData, setUserData] = useState(blankData);
-    const [formErrors, setFormErrors] = useState(errorStrings);
-    const [disabled, setDisabled] = useState(false);
-    // const [userList, setUserList] = useState([]);
+    const [userData, setUserData] = useState({
+        username: "",
+        password: ""
+    });
+    const [disabled, setDisabled] = useState(true);
+    const [formErrors, setFormErrors] = useState({ username: "", password: "" });
+
+
     let history = useHistory();
 
-    const change = (evt) =>
+    const change = (e) =>
     {
-        const { name, value } = evt.target;
+        const { name, value } = e.target;
         validate(name, value);
         setUserData({ ...userData, [name]: value });
     };
+
 
     const submit = (evt) =>
     {
@@ -36,21 +87,19 @@ function Login({ setLoggedIn, setIsOrganizer })
             .then((res) =>
             {
                 localStorage.setItem("token", res.data.token);
-                localStorage.setItem("organizer", true);
-                setLoggedIn(true)
-                setIsOrganizer(true)
-                setUserData(blankData)
-                history.push("/events");
+                history.push("/protected");
             })
+            .catch((err) =>
+            {
+                console.log(err);
+            })
+            .finally(() =>
+            {
+                setUserData('');
+                window.location.reload()
+            });
     };
 
-    // const addUser = () => {
-    //   setUserList([...userList, userData]);
-    // };
-
-    // useEffect(() => {
-
-    // }, [userList]);
 
     const validate = (name, value) =>
     {
@@ -74,51 +123,52 @@ function Login({ setLoggedIn, setIsOrganizer })
             });
     };
 
-    // useEffect(() =>
-    // {
-    //     schema.isValid(userData).then((valid) =>
-    //     {
-    //         setDisabled(!valid);
-    //     });
-    // }, [userData]);
+
+    useEffect(() =>
+    {
+        schema.isValid(userData).then((valid) =>
+        {
+            setDisabled(!valid);
+        });
+    }, [userData]);
+
+
+
 
     return (
-        <>
-            <div className="formBG">
-                <div className="loginBox">
-                    <div className="mainForm">
-                        <h2>Login</h2>
-                        <form onSubmit={submit}>
-                            <div className="item">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={userData.username}
-                                    onChange={change}
-                                />
-                            </div>
-                            <p className="errorMsg">{formErrors.username}</p>
-                            <div className="item">
-                                <input
-                                    type="text"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={userData.password}
-                                    onChange={change}
-                                />
-                            </div>
-                            <p className="errorMsg">{formErrors.password}</p>
-                            <div className="itemSub">
-                                <button id="subutton" disabled={disabled}>
-                                    Login
-                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+
+        <Container className=".logincontainer">
+            <Col>
+                <Card className='loginCard'>            <h1 className='loginHeader'>Sign in to your account</h1>
+                    <Form onSubmit={submit}>
+                        <Input
+                            type='text'
+                            name='username'
+                            placeholder='Username'
+                            className='loginInput'
+                            value={userData.username}
+                            onChange={change}
+                        />
+
+                        <p className="errorMsg">{formErrors.username}</p>
+
+
+                        <Input
+                            type='password'
+                            name='password'
+                            placeholder='Password'
+                            className='loginInput'
+                            value={userData.password}
+                            onChange={change}
+                        />
+
+                        <Button className='loginButton' type='submit'>Login </Button>
+                    </Form>
+                </Card>
+            </Col>
+        </Container>
+
+    )
 }
+
 export default Login 
